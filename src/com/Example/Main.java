af2b72811cf1001f5969b51f8bd6b25f972f2e5f
[Patrick <patsricksilva@gmail.com>
2022-12-11 16:47:41 -0500

    removed comments

--- src/com/Example/Main.java
+++ src/com/Example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	Account TDBank = new Account("123456789", 1234.99, "Patrick Silva", "tdbank@gmail.com", "4168869243");
-//    TDBank.DepositMoney(1000);
+    TDBank.DepositMoney(1000);
     TDBank.WithDraw(500);
     }
 }
