d7156392460aeaeaf934386ea50044c99698428d
[Patrick <patsricksilva@gmail.com>
2022-12-10 21:15:39 -0500

    Initial commit

---
+++ src/com/Example/Main.java
@@ -0,0 +1,10 @@
+package com.Example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Account TDBank = new Account("123456789", 0.00, "Patrick Silva", "tdbank@gmail.com", "4168869243");
+//    TDBank.DepositMoney(1000);
+    TDBank.WithDraw(500);
+    }
+}
